97709458986c9fa99d6d96af32eeea6a9636a85a
zhang <523213189@qq.com>
2017-04-15 12:50:10 +0800

    改用PIL模块

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
 # -*- coding: cp936 -*-
 from socket import *
 import struct, os
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PIL import Image
 
-HOST = '10.25.79.49'
+HOST = '10.22.51.193'
 PORT = 12345
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
@@ -42,7 +41,5 @@ print "Received Successful!"
 fp.close()
 client.close()
 
-lena = mpimg.imread(filename)
-plt.imshow(lena)
-plt.axis('off')
-plt.show()
+im = Image.open(filename)
+im.show()
